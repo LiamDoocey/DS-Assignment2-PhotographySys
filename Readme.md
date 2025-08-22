2e4b211040a58d68cd2574318c9586c875b158c5
Liam Doocey <ljdoocey@gmail.com>
2025-08-22 22:44:20 +0100

    Init App

--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
-Test
+DS Assignment 2 - Photography System
+
---
+++ photography-app/test/photography-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PhotographyApp from '../lib/photography-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/photography-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PhotographyApp.PhotographyAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
